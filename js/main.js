b2ec10c5d59a1f45be06de181f7babcb0db932ee
andrea7896 <andrea_nataly_05@hotmail.com>
2020-03-01 11:42:29 -0600

    finished

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var i = 0;
+var txt = 'Interested in technology, programming, graphic design, and photography.'; 
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
